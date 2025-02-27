625052dd671fb8352e70fa138d5430a5d8d8ccdd
mubashir hafeez awan <mubashir.hafeez.official07@gmail.com>
2025-02-26 21:56:08 -0800

    Add files via upload

---
+++ lab 14 task of pf.cpp
@@ -0,0 +1,401 @@
+//#include<iostream>
+//using namespace std;
+//struct student
+//{
+//	string first_name;
+//	string last_name;
+//	int roll_no;
+//	int marks;
+//	void displayfunction(student student){
+//	
+//	cout<<"first name"<<student.first_name<<endl;
+//	cout<<"last name"<<student.last_name<<endl;
+//	cout<<"roll no"<<student.roll_no<<endl;
+//	cout<<"marks"<<student.marks<<endl;
+//	}
+//};
+//	int main(){
+//
+//	student s1;
+//	s1.first_name="abid";
+//	s1.last_name="ali";
+//	s1.roll_no= 4;
+//	s1.marks= 1200;
+//	s1.displayfunction(s1);
+//}
+//#include <iostream>
+//using namespace std;
+//struct student{
+//	string first_name;
+//	string last_name;
+//	int marks;
+//	int total_marks;
+//	int roll_number;
+//	int n;
+//	void displayfunction(student student){
+//		cout<<"First name: "<<student.first_name<<endl;
+//		cout<<"Last name: "<<student.last_name<<endl;
+//		cout<<"Roll number: "<<student.roll_number<<endl;
+//		cout<<"Marks: "<<student.marks<<" / "<<student.total_marks<<endl;
+//	}
+//};
+//int main(){
+//	student s1;
+//	student arr[s1.n];
+//	cout<<"Enter the number to how many store student data: ";
+//	cin>>s1.n;
+//		cout<<"Enter the first name: ";
+//		cin>>s1.first_name;
+//		cout<<"Enter the last name: ";
+//		cin>>s1.last_name;
+//		cout<<"Enter the roll number: ";
+//		cin>>s1.roll_number;
+//		cout<<"Enter the marks: ";
+//		cin>>s1.marks;
+//		cout<<"Enter the total marks: ";
+//		cin>>s1.total_marks;
+//		s1.displayfunction(s1);
+//	for(int i=0;i<s1.n-1;i++){
+//		cout<<"Enter the first name: ";
+//		cin>>arr[i].first_name;
+//		cout<<"Enter the last name: ";
+//		cin>>arr[i].last_name;
+//		cout<<"Enter the roll number: ";
+//		cin>>arr[i].roll_number;
+//		cout<<"Enter the marks: ";
+//		cin>>arr[i].marks;
+//		cout<<"Enter the total marks: ";
+//		cin>>arr[i].total_marks;
+//		arr[i].displayfunction(arr[i]);
+//	}
+//}
+
+//
+//#include<iostream>
+//using namespace std;
+//struct student
+//{
+//	string first_name;
+//	string last_name;
+//	int roll_no;
+//	int marks;
+//	void displayfunction(student *ptr){
+//	
+//	cout<<"first name"<<ptr->first_name<<endl;
+//	cout<<"last name"<<ptr->last_name<<endl;
+//	cout<<"roll no"<<ptr->roll_no<<endl;
+//	cout<<"marks"<<ptr->marks<<endl;
+//	}
+//};
+//	int main(){
+//
+//	student s1;
+//	s1.first_name="abid";
+//	s1.last_name="ali";
+//	s1.roll_no= 4;
+//	s1.marks= 1200;
+//	s1.displayfunction(&s1);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
