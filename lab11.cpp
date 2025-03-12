74fec042b0b291c491b96f631af60eccce5ddb97
mubashir hafeez awan <mubashir.hafeez.official07@gmail.com>
2025-03-12 05:48:25 -0700

    Add files via upload

---
+++ lab11.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[5]={10,20,30,40,50},i;
+	for(i=0;i<5; i++){
+	
+	cout<<arr[i]<<endl;}
+}
+//progrm 2//:
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter an array"<<endl;
+//	cin>>n;
+//	int arr[n] ,i;
+//	for(i=0;i<n;i++){
+//	
+//	cin>>arr[i];
+//}
+//	for(i=0;i<n;i++){
+//
+//	cout<<arr[i]<< i<<endl;
+//	}
+//	for(i=n-1;i>=0;i--){
+//	
+//	cout<<arr[i]<<i<<endl;}
+//	
+//}
+//program #3//:
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int sum=0,length,i ;
+//	double average;
+//	int arr[5]={10,20,30,40,50};
+//	for(i=0;i<5;i++){
+//	
+//	sum+=arr[i];
+//	average=sum/5;
+//}
+//	cout<<sum<<endl;
+//	cout<<"the average"<<average<<endl;
+//}
+// program 4//:
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int length,largest,i;
+//	cout<<" lenght of array"<<endl;
+//	cin>>length;
+//	int array[length];
+//	cout<<"enter array according to length"<<endl;
+//	for(i=0;i<length;i++){
+//		cin>>array[i];
+//	}
+//	largest=array[0];
+//	for(i=0;i<length;i++){
+//		if(array[i]>largest){
+//			largest=array[i];
+//		}
+//	}
+//cout<<"the largest number"<<largest<<endl;	
+//	
+//}
+/// program 5//:
+//#include<iostream>
+//using namespace std;
+//int main(){
+//
+//int arrlength,arrsize;
+//int array[5]={1,2,3,4,5};
+//arrlength=sizeof(array)/sizeof(array[0]);
+//arrsize=sizeof(array);
+//cout<<arrlength<<endl;
+//cout<<arrsize;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
